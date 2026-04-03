0b2402b0ee22f6168cb9d0b1022b073dc6cbdf8e
ItsOnlyMe01 <134477804+ItsOnlyMe01@users.noreply.github.com>
2026-04-03 15:55:43 +0530

    fixed connection issue with frontend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const courseRoutes = require("./routes/CourseRoute");
 const documentRoutes = require("./routes/DocumentRoute");
@@ -11,8 +12,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 3000;
 const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 3000;
 
 app.use("/courses", courseRoutes);
 app.use("/documents", documentRoutes);
@@ -23,9 +24,20 @@ mongoose
   .then(() => console.log("connected to db"))
   .catch((err) => console.error("Failed to connect", err));
 
-app.get("/", (req, res) => {
-  res.send("server is running");
+// app.get("/", (req, res) => {
+//   res.send("server is running");
+// });
+
+//-------------deployment-------------------------------------------
+
+const __dirnamePath = path.resolve();
+
+app.use(express.static(path.join(__dirnamePath, "../frontend/dist")));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirnamePath, "../frontend/dist/index.html"));
 });
+
+//-------------------------------------------------------------------
 
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
